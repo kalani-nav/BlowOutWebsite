98e8bcc42dffad970c1e5a8a6afdf371f7b1943c
Brandon Navarro <55892338+kalani-nav@users.noreply.github.com>
2019-11-13 17:21:13 -0700

    Add project files.

---
+++ BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.FirstMessage = "Please call Support at <strong><u>801-555-1212</u></strong>. Thank you!";
+            return View();
+        }
+
+        public ActionResult Email(string name, string email)
+        {
+            ViewBag.Response = "Thank you " + name + ". We will send an email to " + email + ".";
+            return View();
+        }
+    }
+}
---
+++ BlowOut/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Rentals()
+        {
+            ViewBag.SelectInstrument = "Select Instrument";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        //------------------------------------------------------------
+
+        //INSTRUMENT METHODS
+
+        public ActionResult Trumpet()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Trombone()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Tuba()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Flute()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Clarinet()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Saxophone()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
